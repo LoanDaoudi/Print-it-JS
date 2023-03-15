6adcd1af1f6138ef7f9ed6b02f9912a385b7aa0e
LoanDaoudi <loanmateo.daoudi@gmail.com>
2023-03-15 10:37:08 +0100

    2nd cahnge

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,4 @@
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+
 document.addEventListener("DOMContentLoaded", ()=>{
 
 
@@ -43,11 +26,29 @@ document.addEventListener("DOMContentLoaded", ()=>{
 	let slide = 0;
 	
 	function affichedot(slide){
-		console.log ("./assets/images/slideshow/"+ slides[slide].image);
+		
+		
+		
 		fichier=chemin+slides[slide].image;
 		$(".banner-img").attr("src", fichier);
-	
+		$("#banner p").html(slides[slide].tagLine);
+		$(".dots").empty();
+		for (let i = 0;  i < slides.length ; i++){
+			if (i == slide) {
+				$(".dots").append(
+					$("<div>").prop({
+						class: "dot dot_selected"
+					})
+				)
+			}else{ 
 		
+				$(".dots").append(
+					$("<div>").prop({
+						class: "dot "
+					})
+				)
+			}
+			}
 	}
 	 
 	document.querySelector (".arrow_left").addEventListener ("click", ()=> {
@@ -62,5 +63,5 @@ document.addEventListener("DOMContentLoaded", ()=>{
 			if (slide<slides.length-1){slide++;}else{slide=0;}
 			affichedot(slide);
 			})
-		})
+})
 	
